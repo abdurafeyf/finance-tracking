2f6a02cf1a0fbc7fc62476e31f2950306342a3e1
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-10-02 11:16:00 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
-import './css/style.css';
 import './charts/ChartjsConfig';
 import Hello from "./pages/Hello";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
+    <div className="h-[100vh]">
       <Routes>
         <Route exact path="/" element={<Dashboard/>} />
         <Route exact path="/hello" element={<Hello/>} />
+        <Route exact path="/login" element={<Login/>} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import withMT from "@material-tailwind/react/utils/withMT";
+export default withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'skygreen': '#6CE606'
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('flowbite/plugin'),
+    require('daisyui')
+  ],
+});

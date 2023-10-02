2a967e77914c2f2cd3746d69b1534b7a3a75f007
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-10-02 11:26:17 +0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,119 @@
-import React from "react";
-import './App.css';
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import GoogleButton from "react-google-button";
+import { LoginSuccess } from "./containers/LoginSuccess";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthUser, setIsAuthenticated } from "./redux/appSlice";
 import Dashboard from './pages/Dashboard';
 import './charts/ChartjsConfig';
 import Hello from "./pages/Hello";
-import Login from "./pages/Login";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 31px;
+`;
 
 function App() {
+  const user = useSelector((state) => state.app.authUser);
+  console.log('rafey:', user);
+  const isAuthenticated = useSelector((state) => state.app.isAuthenticated);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    fetchAuthUser();
+  }, []);
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]);
+
+  const fetchAuthUser = async () => {
+    const response = await axios
+      .get("https://fin-tracking-backend.vercel.app/api/v1/auth/user", { withCredentials: true })
+      .catch((err) => {
+        console.log("Not properly authenticated");
+        dispatch(setIsAuthenticated(false));
+        dispatch(setAuthUser(null));
+      });
+
+    if (response && response.data) {
+      console.log("User: ", response.data);
+      dispatch(setIsAuthenticated(true));
+      dispatch(setAuthUser(response.data));
+    }
+  };
+
+  const redirectToGoogleSSO = async () => {
+    const googleLoginURL = "https://fin-tracking-backend.vercel.app/api/v1/login/google";
+    const newWindow = window.open(
+      googleLoginURL,
+      "_blank",
+      "width=500,height=600"
+    );
+  
+    if (!newWindow) {
+      console.error("Failed to open Google SSO window");
+      return;
+    }
+  
+    try {
+      await new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+          clearInterval(timer);
+          reject(new Error("Authentication timed out"));
+        }, 60000); // Set a reasonable timeout (e.g., 60 seconds)
+  
+        const timer = setInterval(() => {
+          if (newWindow.closed) {
+            clearInterval(timer);
+            clearTimeout(timeout);
+            console.log("Yay, we're authenticated");
+            fetchAuthUser();
+            resolve();
+            navigate('/welcome');
+          }
+        }, 500);
+      });
+    } catch (error) {
+      console.error("Authentication error:", error);
+      navigate('/login/error');
+    }
+  };  
+
   return (
-    <div className="h-[100vh]">
+    <>
       <Routes>
         <Route exact path="/" element={<Dashboard/>} />
         <Route exact path="/hello" element={<Hello/>} />
-        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/login" element={<GoogleButton onClick={redirectToGoogleSSO} />} />
+        <Route
+          path="/welcome"
+          element={
+            isAuthenticated ? (
+              <div>Welcome Back {user && user.fullName}</div>
+            ) : (
+              <div>Loading...</div>
+            )
+          }
+        />
+        <Route path="/login/success" element={<LoginSuccess />} />
+        <Route path="/login/error">Error logging in. Please try again later!</Route>
       </Routes>
-    </div>
+    </>
   );
+  
+
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function NavBar() {
                 <li><a>Item 3</a></li>
             </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <a className="btn btn-ghost normal-case text-xl" href="/">NuvexaFinance</a>
         </div>
         <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
